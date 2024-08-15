55a4e127b2cb953ad9a956de601e6f656d3aa955
Technology Rooms <technologyrooms@colsanjose.edu.co>
2024-08-15 14:40:15 -0500

    mi primer

---
+++ main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String args[]){
+        int x = 0;
+        float y = 3.141592653589793238f;
+        double z = 1000;
+        boolean w = false;
+        boolean v = false;
+        System.out.print(x);
+        System.out.println("hello world");
+
+        if(w==v){
+            v = true;
+        }
+    }
+}
